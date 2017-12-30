b88d750ab68422784d6f3e5a3fffce2c8861d9d7
Andy Maskiell <maskiella@gmail.com>
2017-12-30 19:39:20 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,7 @@ class EmailParser
   end
 
   def parse
-    parsed_emails = @emails.split(/\,|(\,\s)|\s/)
+    parsed_emails = @emails.split(/\,|\s/)
     parsed_emails.each do |array_element|
       @@all << array_element if (array_element != "" && !(@@all.include?(array_element)))
     end
